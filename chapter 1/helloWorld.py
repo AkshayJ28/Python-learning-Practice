3dca6188e33522992cffc2098323018b630a756b
Akshay Jain <akshayjain2807@gmail.com>
2021-05-30 20:05:53 +0530

    helloWorld.py file added.

---
+++ chapter 1/helloWorld.py
@@ -0,0 +1,4 @@
+print('Hello world !!') #single quote
+print("Hello World !!") #double quete
+print("Hello 'world' my world") #single quote inside double quote
+print('Hello "world" my best world') #double qoute inside single qoute.
